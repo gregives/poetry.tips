0e34f43312ba0a0f3ea13cf075bee28061eb3c88
Greg Ives <greg@gregives.co.uk>
2023-05-04 00:11:38 +0100

    Add more articles (#13)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,9 @@ export const articles: string[] = [
   "sonnet-poem-generator-how-to-write-your-own-shakespearean-sonnet",
   "limerick-poem-generator-a-playful-and-witty-poetry-style",
   "the-pros-and-cons-of-using-a-poem-generator-for-poetry-writing",
+  "love-poem-generator-create-romantic-poems-in-seconds",
+  "the-evolution-of-poetry-writing-from-pen-and-paper-to-ai-poem-generators",
+  "the-ethics-of-ai-poetry-should-we-consider-ai-generated-poems-as-art",
 ];
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
