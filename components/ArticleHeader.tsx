0e3a63727354fc88be7e84a1c870562976754c41
Greg Ives <greg@gregives.co.uk>
2023-05-04 18:00:53 +0100

    Improve style of articles (#18)

--- components/ArticleHeader.tsx
+++ components/ArticleHeader.tsx
@@ -15,16 +15,8 @@ export type Metadata = {
 export function ArticleHeader({ metadata }: { metadata: Metadata }) {
   return (
     <>
-      <div className="relative w-full rounded-3xl aspect-[16/9] sm:aspect-[2/1] mb-8 bg-gray-50">
-        <Image
-          src={metadata.image}
-          alt=""
-          fill
-          className="object-cover rounded-3xl"
-        />
-        <div className="absolute inset-0 rounded-3xl shadow-inner bg-gradient-to-br from-white/20" />
-      </div>
-      <div className="flex flex-wrap items-center">
+      <Heading1>{metadata.title}</Heading1>
+      <div className="flex flex-wrap items-center mb-4">
         <time
           dateTime={new Date(metadata.date).toISOString()}
           className="w-full sm:w-auto text-sm md:text-base text-gray-500 mr-6 mb-2"
@@ -36,13 +28,21 @@ export function ArticleHeader({ metadata }: { metadata: Metadata }) {
         {metadata.tags.map((tag) => (
           <div
             key={tag}
-            className="rounded-lg px-2 py-1 bg-gray-50 text-gray-600 text-sm md:text-base -ml-1 mr-3 mb-2"
+            className="rounded-lg px-2 py-1 bg-gray-100 text-gray-700 text-sm md:text-base -ml-1 mr-3 mb-2"
           >
             {tag}
           </div>
         ))}
       </div>
-      <Heading1>{metadata.title}</Heading1>
+      <div className="relative w-full rounded-3xl aspect-[16/9] sm:aspect-[2/1] mb-8 sm:mb-16 bg-gray-50">
+        <Image
+          src={metadata.image}
+          alt=""
+          fill
+          className="object-cover rounded-3xl"
+        />
+        <div className="absolute inset-0 rounded-3xl shadow-inner bg-gradient-to-br from-white/20" />
+      </div>
       <Paragraph>{metadata.description}</Paragraph>
     </>
   );
