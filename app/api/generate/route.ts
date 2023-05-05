156ae7d214358324ec933777c85bd0a33b92fd4f
Greg Ives <greg@gregives.co.uk>
2023-05-05 15:39:46 +0100

    Generate poem in Edge function

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,72 +1,26 @@
 import { createOpenAIStream } from "@/utilities/createOpenAIStream";
-import { firestore } from "@/utilities/firestore";
-import { getServerSession } from "@/utilities/getServerSession";
 import { NextRequest, NextResponse } from "next/server";
-import { FieldValue } from "firebase-admin/firestore";
-import { Options, Poem } from "@/types";
+import { Options } from "@/types";
 
 export async function POST(request: NextRequest) {
-  const session = await getServerSession();
-  const email = session?.user?.email;
-
-  if (typeof email !== "string") {
-    return;
-  }
-
-  const query = firestore
-    .collection("users")
-    .where("email", "==", email)
-    .limit(1)
-    .get();
-
   const options: Options = await request.json();
 
   const content = `Write a ${
     options.type === "Random Poem" ? "poem" : options.type.toLowerCase()
   } about ${options.prompt}`;
 
-  const onClose = async (response: string) => {
-    const {
-      docs: [user],
-    } = await query;
-
-    if (user.data().credits === 0) {
-      return;
-    }
-
-    const credits =
-      user.data().credits === "Unlimited"
-        ? "Unlimited"
-        : (user.data().credits ?? 5) - 1;
-
-    const poem: Poem = {
-      options,
-      response,
-      createdAt: Date.now(),
-    };
-
-    await firestore
-      .collection("users")
-      .doc(user.id)
-      .update({
-        credits,
-        poems: FieldValue.arrayUnion(poem),
-      });
-  };
-
-  const stream = await createOpenAIStream(
-    {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content,
-        },
-      ],
-      stream: true,
-    },
-    onClose
-  );
+  const stream = await createOpenAIStream({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content,
+      },
+    ],
+    stream: true,
+  });
 
   return new NextResponse(stream);
 }
+
+export const runtime = "edge";
