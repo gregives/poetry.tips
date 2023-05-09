6c0c8e850812ecf81ba51a2ddd82a6996b1b614a
Greg Ives <greg@gregives.co.uk>
2023-05-09 11:13:06 +0100

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { getServerSession } from "@/utilities/getServerSession";
 import { generateMetadataFromSlug } from "@/utilities/generateMetadataFromSlug";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,6 +29,18 @@ export default async function RootLayout({
           <Header />
           <main className="flex-1">{children}</main>
           <Footer />
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-DQVQT5DW9P"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DQVQT5DW9P');
+            `}
+          </Script>
         </body>
       </html>
     </SessionProvider>
