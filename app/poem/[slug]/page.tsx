b9baa719d8a6cc5a3459b59f1d725215053ab88a
Greg Ives <greg@gregives.co.uk>
2023-05-13 13:36:55 +0100

    Add social proof (#32)

--- app/poem/[slug]/page.tsx
+++ app/poem/[slug]/page.tsx
@@ -28,6 +28,11 @@ export default function PoemPage({
   return (
     <Container className="pt-16 pb-24">
       <HeroPattern className={poemType.classNames.background} />
+      <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+        <div className="-my-2 relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 backdrop-blur-lg">
+          More than 1,000 poems generated
+        </div>
+      </div>
       <h1
         className={twMerge(
           "sm:text-center text-4xl/snug sm:text-5xl/snug md:text-6xl/snug font-bold tracking-tight mb-16 sm:mb-24 lg:mb-32",
